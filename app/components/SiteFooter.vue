d18cb996c6dd16069657e62c4bd6778f1aa35dbc
Etherrreal. <65111206+lifeafter619@users.noreply.github.com>
2025-09-10 22:36:51 +0800

    Update SiteFooter.vue

--- app/components/SiteFooter.vue
+++ app/components/SiteFooter.vue
@@ -32,7 +32,7 @@
       </n-p>
       <n-p depth="3">
         Copyright &copy; 2020 - {{ new Date().getFullYear() }}
-        <n-text depth="3" @click="jumpLink(linkData.home)"> 619 </n-text>
+        <n-text depth="3" @click="jumpLink(linkData.home)"> 𝟞𝟙𝟡'𝕤 𝔹𝕃𝕆𝔾 </n-text>
         <n-text
           v-if="siteIcp"
           depth="3"
